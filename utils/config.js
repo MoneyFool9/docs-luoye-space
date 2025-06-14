70e0c6e26dcd8803cc656be2edc29302dd36b959
钱多多 <14382551+Moneyfool@user.noreply.gitee.com>
2025-06-14 10:05:40 +0800

    更新config

--- utils/config.js
+++ utils/config.js
@@ -4,27 +4,28 @@ export const textAndIndexMap = {
     'front-end': { name: '前端', index: 1000 },
     'big-front-end': { name: '大前端', index: 888 },
     'fundamental-concept': { name: '基础概念', index: 666 },
-    'problems': { name: '问题', index: 555 },
+    'dataStruct_algo': { name: '算法', index: 555 },
     'temp': { name: '施工中', index: 0 },
   
     // front-end的子目录
+    'HTML_CSS': { name: 'HTML和CSS', index: 2222 },
     'JavaScript': { name: 'JavaScript', index: 2000 },
     'ES6': { name: 'ES6', index: 1999 },
     'Vue': { name: 'Vue', index: 1500 },
     'React': { name: 'React', index: 1400 },
     'Typescript': { name: 'Typescript', index: 1333 },
     // 'interview': { name: '面试', index: 1300 },
-    'HTML_CSS': { name: 'HTML和CSS', index: 1000 },
+    'Front-end_Engineering': { name: '前端工程化', index: 999 },
     'bugfix': { name: 'BUG解决方案', index: 666 },
     // 'read': { name: '读书笔记', index: 555 },
-    // 'some-library': { name: '一些库', index: 444 },
+    'some-library': { name: '一些库', index: 444 },
   
     //big-front-end的子目录
     'wx-miniprogram': {name: '微信小程序', index: 2000},
     'uniapp': {name: 'uniapp', index: 1999},
     'nodejs': {name: 'node.js学习', index: 1999},
 
-    // problems的子目录
+    // dataStruct_algo的子目录
     'algorithm': { name: '算法', index: 1000 },
     'lanqiao': { name: '蓝桥杯', index: 900 },
   
